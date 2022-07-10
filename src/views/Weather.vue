43696f4617cf9b5ea145ddc786c60569686e23d9
Judielle Villa <judiellezinke@yahoo.de>
2022-07-11 00:20:24 +0200

    added inputbar and button for weather component

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -1,11 +1,29 @@
 <template>
     <div class="container title-container">
         <h1>Weather</h1>
+        <div class="container weather-div">
+            <input type="text" class="weather-city-input rounded-pill px-2" placeholder="Enter City Name" >
+            <button class="btn btn-primary mx-1">Go</button>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'WeatherPage'
+        name: 'WeatherPage',
+        data () {
+            return {
+
+            }
+        }
     }
 </script>
+
+<style scoped>
+    .weather-div {
+        margin-top: 2rem;
+        display: flex;
+        justify-content: center;
+    }
+
+</style>
