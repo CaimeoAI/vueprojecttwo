a5977630dcc425865e2083c71eef5c758a81f785
Judielle Villa <judiellezinke@yahoo.de>
2022-07-11 10:23:15 +0200

    added weather information placeholder and styling to weather component

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -5,6 +5,17 @@
             <input type="text" class="weather-city-input rounded-pill px-2" placeholder="Enter City Name" >
             <button class="btn btn-primary mx-1">Go</button>
         </div>
+        <div class="container weather-display">
+            <div class="location-box">
+                <div class="location">Cologne, Germany</div>
+                <div class="date">Monday 20 January 2022</div>
+            </div>
+
+            <div class="weather-info">
+                <div class="temp">21°C</div>
+                <div class="weather">Cloudy</div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,7 +24,7 @@
         name: 'WeatherPage',
         data () {
             return {
-
+                api_key: 'ff4424768-00ed-11ed-93b0-0242ac130002-f44247cc-00ed-11ed-93b0-0242ac130002'
             }
         }
     }
@@ -24,6 +35,56 @@
         margin-top: 2rem;
         display: flex;
         justify-content: center;
+
     }
 
+    .weather-display {
+        margin-top: 1rem;
+        height: 500px;
+        width: 350px;
+        border-radius: 25px;
+        background-color: steelblue;
+    }
+
+    .location {
+        color: #FFF;
+        font-size: 32px;
+        font-weight: 500;
+        text-align: center;
+        text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+    }
+
+    .date {
+        color: #FFF;
+        font-size: 20px;
+        font-weight: 300;
+        text-align: center;
+        text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+    }
+
+    .weather-info {
+        text-align: center;
+    }
+
+    .temp {
+        display: inline-block;
+        padding: 10px 25px;
+        color: #FFF;
+        font-weight: 900;
+        font-size: 100px;
+
+        text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+        background-color: rgba(255,255,255, 0.25);
+        border-radius: 20px;
+        margin: 30px 0;
+        box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    }
+    
+    .weather {
+        color: #FFF;
+        font-size: 48px;
+        font-weight: 700;
+
+        text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+    }
 </style>
